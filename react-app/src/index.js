666d68ac076956b741ef35b0695104acadc2ffa7
denis23somfalean <denissomfalean@gmail.com>
2023-01-07 19:39:48 +0200

    added icons

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App/App.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
