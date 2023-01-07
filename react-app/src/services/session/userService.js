e6de5c422dc06f65331bcbb990f3b7178ee6f35d
denis23somfalean <denissomfalean@gmail.com>
2023-01-08 00:59:32 +0200

    adding the logged user to the storage

--- react-app/src/services/session/userService.js
+++ react-app/src/services/session/userService.js
@@ -4,14 +4,18 @@ export function areCredentialsCorrect(email, password) {
   let users = getSessionItem(SessionStorageKeys.USERS);
   
   if (users === null) {
+    
     return false;
   }
 
   let loggedUser = users.find(user => user.email === email && user.password == password);
 
   if(loggedUser === undefined){
+    
     return false;
   }
+
+  setSessionItem(SessionStorageKeys.USER, loggedUser);
 
   return true;
 }
